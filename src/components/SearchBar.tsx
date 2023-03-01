974c1895084e5a9d4f01175e25d3a0dd932a3106
arascic <arascic1@etf.unsa.ba>
2023-03-01 23:11:03 +0100

    dodana ikonica za search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import './css/search_bar.css';
 
 const SearchBar = () => {
   return (
     <div id = "searchBarDiv">
+        <FontAwesomeIcon className="ikonica" icon={faMagnifyingGlass}/>
         <input id = "searchBar" type = "text" placeholder = "Search..."></input>
     </div>
   )
